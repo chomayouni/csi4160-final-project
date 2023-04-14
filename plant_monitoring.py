229228c4574c14a4a694237a618c1ae3e6635800
chomayouni <69778851+chomayouni@users.noreply.github.com>
2023-04-14 13:19:23 -0400

    Update plant_monitoring.py

--- plant_monitoring.py
+++ plant_monitoring.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 import time
+import sched
 
 
 # this will trigger the lights/ anything hooked up to the IOT
@@ -16,6 +17,7 @@ def ioton():
 
 # this will turn off the lights/ anything hooked up to the IOT
 def iotoff():
+    #This initializes the GPIO pin that will be used ot activate the IOT switch
     PIN_GPIO = 17
  
     GPIO.setwarnings(False)
@@ -67,9 +69,10 @@ def moisture():
     while True:
             time.sleep(1)
 
-
+#This function when called will prompt the users to enter a request 
+#This is meant to run tandam to the run_daily_schedule function and allows manual manipulation.
 def command():
-     command = str.upper(input("Please enter START, STOP, or STATUS to get an update: "))
+     command = str.lower(input("Please enter START, STOP, or STATUS to get an update: "))
 
      if command == "start":
         print("Starting the plant monitoring system. Please stand by for readings.")
@@ -84,6 +87,48 @@ def command():
         moisture()
      else:
         print('INVALID INPUT: Please enter START, STOP, or STATUS to get an update: ')
+
+# This uses python's bulit in schedular to activate the IOT at sceduled times.
+def run_daily_schedule():
+    # Initialize the scheduler
+    scheduler = sched.scheduler(time.time, time.sleep)
+    
+    # Define the function to be run at 8am
+    def run_at_8am():
+        print("The lights and fan were turned on at 8am!")
+        ioton()
+        
+        scheduler.enterabs(get_next_run_time(17), 1, run_at_5pm) #Sets the 5pm scedule
+
+    # Define the function to be run at 5pm
+    def run_at_5pm():
+        print("Scheduled shutdown has happened at 5pm!")
+        iotoff()
+        
+        scheduler.enterabs(get_next_run_time(8), 1, run_at_8am) #sets the 8am schedule
+
+    # Schedule the first run of the function at 8am
+    scheduler.enterabs(get_next_run_time(8), 1, run_at_8am)
+    
+    # Start the scheduler
+    scheduler.run()
+
+def get_next_run_time(hour):
+    current_time = time.time()
+    current_datetime = time.localtime(current_time)
+
+    # Set the hour of the next run time to the specified hour
+    next_run_datetime = time.struct_time((current_datetime.tm_year, current_datetime.tm_mon, current_datetime.tm_mday, hour, 0, 0, current_datetime.tm_wday, current_datetime.tm_yday, current_datetime.tm_isdst))
+    next_run_time = time.mktime(next_run_datetime)
+    
+    # If the next run time is in the past, add a day to the next run time
+    if next_run_time < current_time:
+        next_run_time += 86400
+    
+    return next_run_time
+
+     
+     
 
 #this function will run at the beginning of the program and start prompting the user
 command()
